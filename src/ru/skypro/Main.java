c9ebd3f51d2e9ee82535297363ef9d0c1dd9192b
vivatgreen <grindsasha@bk.ru>
2022-03-04 11:31:01 +0300

    Commit

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -3,6 +3,20 @@ package ru.skypro;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	// Задание 1
+        byte horse = 1;
+        short cat = 2;
+        int cow = 3;
+        long duck = 1122L;
+        var snake = 127;
+        float lionWeight = 135.5f;
+        double elephantWeight = 1.2564;
+        boolean uncleIsAdult = 45 < 55;
+        System.out.println(uncleIsAdult);
+        char name = 22;
+        System.out.println(name);
+
+
+
     }
 }
