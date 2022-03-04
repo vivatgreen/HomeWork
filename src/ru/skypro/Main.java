9c804db3587a674d512a6ec49d49269a0941b475
vivatgreen <grindsasha@bk.ru>
2022-03-04 16:09:41 +0300

    Commit

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -24,5 +24,39 @@ public class Main {
         double boxersWeightDifference = boxerTwoWeight - boxerOneWeight;
         System.out.println("Разница весов между боксёрами " + boxersWeightDifference + " кг!");
 
+        // Задание 3
+        int bananas = 5;
+        int oneBananasWeightGr = 80;
+        int allBananasWeightGr = bananas * oneBananasWeightGr;
+        int milkWeightMl = 200;
+        int milk100MlPerGr = 105;
+        int allMilkWeightGr = milk100MlPerGr * 2;
+        int iceCreamSundaeBricks = 2;
+        int oneIceCreamSundaeBrickWeightGr = 100;
+        int iceCreamSundaeBricksWeightGr = iceCreamSundaeBricks * oneIceCreamSundaeBrickWeightGr;
+        int rawEggs = 4;
+        int oneRawEggsWeightGr = 70;
+        int allRawEggsWeightGr = rawEggs * oneRawEggsWeightGr;
+        int allProductsWeightGr = allBananasWeightGr + allMilkWeightGr + iceCreamSundaeBricksWeightGr + allRawEggsWeightGr;
+        int grPerKg = 1000;
+        float allProductsKG = allProductsWeightGr / (float) grPerKg;
+        System.out.println("Общий вес рецепта " + allProductsKG + " кг!");
+
+        // Задание 4
+        int boxerNeedsToLoseWeightKg = 7;
+        int boxerLosesInOneDayWeightGr = 250;
+        int boxerNeedsToLoseWeightGr = boxerNeedsToLoseWeightKg * grPerKg;
+        int itTakesDaysLoseWeight = boxerNeedsToLoseWeightGr / boxerLosesInOneDayWeightGr;
+        System.out.println("Боксёру нужно " + itTakesDaysLoseWeight + " дней чтобы похудеть на 7 кг по 250 гр в день");
+
+        int losesBoxerInOneDayWeightGr = 500;
+        int takesItDaysLoseWeight = boxerNeedsToLoseWeightGr/ losesBoxerInOneDayWeightGr;
+        System.out.println("Боксёру нужно " + takesItDaysLoseWeight + " дней чтобы похудеть на 7 кг по 500 гр в день");
+
+        int onAvarage = (itTakesDaysLoseWeight + takesItDaysLoseWeight) / 2;
+        System.out.println("В среднем потребуется " + onAvarage + " дней для похудения на 7 кг!");
+
+        // Задание 5
+
     }
 }
