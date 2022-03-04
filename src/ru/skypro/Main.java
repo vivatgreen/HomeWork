762d74e51cef08e4037d3d4813c1fc533abb8453
vivatgreen <grindsasha@bk.ru>
2022-03-04 11:44:05 +0300

    Commit

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -5,10 +5,10 @@ public class Main {
     public static void main(String[] args) {
 	// Задание 1
         byte horse = 1;
-        short cat = 2;
-        int cow = 3;
-        long duck = 1122L;
-        var snake = 127;
+        short cats = 2;
+        int cows = 3;
+        long ducks = 1122L;
+        var snakes = 127;
         float lionWeight = 135.5f;
         double elephantWeight = 1.2564;
         boolean uncleIsAdult = 45 < 55;
@@ -16,7 +16,13 @@ public class Main {
         char name = 22;
         System.out.println(name);
 
-
+        // Задание 2
+        double boxerOneWeight = 78.2;
+        double boxerTwoWeight = 82.7;
+        double weightAllBoxers = boxerOneWeight + boxerTwoWeight;
+        System.out.println("Общий вес боксёров равен " + weightAllBoxers + " кг!");
+        double boxersWeightDifference = boxerTwoWeight - boxerOneWeight;
+        System.out.println("Разница весов между боксёрами " + boxersWeightDifference + " кг!");
 
     }
 }
